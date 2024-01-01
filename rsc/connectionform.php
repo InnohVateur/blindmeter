e416f86b6f78158dc708126367189b096b27ec60
InnohVateur <innohvateur@gmail.com>
2024-01-01 16:22:29 +0100

    Fixed bug wrong name for the password input in the connection form

--- rsc/connectionform.php
+++ rsc/connectionform.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="style/login.css">
 <form action="" method="post">
-    <input type="password" name="passwd" placeholder="Password">
+    <input type="password" name="psswd" placeholder="Password">
     <input type="submit" value="Login">
 </form>
