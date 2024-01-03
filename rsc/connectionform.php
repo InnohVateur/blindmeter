0df84e1e438a1f226d19c804da53d726ccf70720
InnohVateur <innohvateur@gmail.com>
2024-01-03 13:11:54 +0100

    Removed unused files

--- rsc/connectionform.php
+++
@@ -1,5 +0,0 @@
-<link rel="stylesheet" href="style/login.css">
-<form action="" method="post">
-    <input type="password" name="psswd" placeholder="Password">
-    <input type="submit" value="Login">
-</form>
